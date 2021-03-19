3510bdb266dc0d46b0557451a06b25961111a106
shili <shili@yizhoucp.cn>
2021-03-19 12:08:14 +0800

    fix: create project bus

--- apps/project/project.py
+++ apps/project/project.py
@@ -10,6 +10,8 @@ Change Activity: 2021/3/18:
 """
 __author__ = 'shili'
 
+from datetime import datetime
+
 from flask_jwt_extended import jwt_required,get_jwt_identity
 from flask_restful import Resource,reqparse
 from apps.models import ProjectModel
@@ -24,18 +26,18 @@ class Project(Resource):
     @jwt_required()
     def post(self):
         """"""
-        self.parse.add_argument("project_name",type=str,require=True,help="项目名称必填")
+        self.parse.add_argument("project_name",type=str,required=True,help="项目名称必填")
         self.parse.add_argument("project_desc",type=str,help="项目描述")
         args = self.parse.parse_args()
         project_name = args.get("project_name")
         project_desc = args.get("project_desc")
         # 获取当前用户ID
         user_id = get_jwt_identity().get("user_id")
-        project_obj = ProjectModel.objects.get(project_name=project_name)
+        project_obj = ProjectModel.objects(project_name=project_name).first()
         if project_obj:
             res = {"code":1000,"message":"项目已存在","data":{}}
             return format_response(res)
-        project = ProjectModel(project_name=project_name,project_desc=project_desc,owner=user_id)
+        project = ProjectModel(project_name=project_name,project_desc=project_desc,owner=user_id,create_time=datetime.utcnow().strftime("%Y-%m%d %H:%M:%S"))
         project.save()
         res = {"code": 0, "message": "success", "data": {"project_id":project.project_id,"project_name":project_name,"project_desc":project_desc}}
         return format_response(res)
